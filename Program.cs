77b0e485a1e5ea141b0e8d85c182ac953b57985a
Alireza Nekobakht <66713408+anekobakht@users.noreply.github.com>
2025-07-25 08:27:05 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using CryptoPriceChecker.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IBinanceService, BinanceService>();
+builder.Services.AddScoped<IAiAnalyzerService, AiAnalyzerService>();
+builder.Services.AddHttpClient(); // برای BinanceService
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); // ← این خیلی مهمه برای نمایش خطا
+}
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Crypto}/{action=Index}/{id?}");
+
+app.Run();
